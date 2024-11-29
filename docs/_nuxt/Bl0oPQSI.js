c8a531b6af9cedad17a552efc868bfcce24376e5
Jeferson Silva <jeferson.i.silva@gmail.com>
2024-11-29 19:20:38 -0300

    build

---
+++ docs/_nuxt/Bl0oPQSI.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./CZ8GDbR_.js";import{_ as n,c as a,b as _,w as d,o as r,d as s}from"./0LDNO9KH.js";const c={};function i(l,e){const t=o;return r(),a("div",null,[_(t,{to:"/modalidade"},{default:d(()=>e[0]||(e[0]=[s("Modalidades")])),_:1})])}const x=n(c,[["render",i]]);export{x as default};
