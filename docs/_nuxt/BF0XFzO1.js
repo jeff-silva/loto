9fbd2a81248762cb7f646333564ccdf3bf322f22
Jeferson Silva <jeferson.i.silva@gmail.com>
2025-01-07 12:01:45 -0300

    feat: melhoria layout site

---
+++ docs/_nuxt/BF0XFzO1.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./CPxALtXB.js";import{_ as n,J as a,t as _,A as s,x as r}from"./BCPlvm8M.js";const c={};function d(i,e){const t=o;return _(),a(t,{name:"site"},{default:s(()=>e[0]||(e[0]=[r("div",null,"Selecione uma Modalidade",-1)])),_:1})}const u=n(c,[["render",d]]);export{u as default};
